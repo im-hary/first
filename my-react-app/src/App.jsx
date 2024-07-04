1b0d1e80e4669bc6a82d7b84e2a4961c3b0bc64e
im-hary <hariharan934526@gmail.com>
2024-07-04 21:46:25 +0530

    deploy react to github

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Tictoe from './components/tictoe'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+
+
+  return (
+   <>
+    <Tictoe/> 
+   
+   </>
+  )
+}
+
+export default App
